28d6753fad78e48b9bf86e32c0740d0d2507bd23
tang joshua <tjh.xbow@qq.com>
2024-05-04 20:20:04 +0800

    add chapter 11

--- chapter10_enums_and_patterns/src/main.rs
+++ chapter10_enums_and_patterns/src/main.rs
@@ -1,3 +1,35 @@
+#[derive(Debug, Clone, Copy)]
+pub enum Ordering {
+    Less = 100,
+    Equal = 200,
+    Greater = 300,
+}
+
+pub fn compare(n: i32, m: i32) -> Ordering {
+    if n < m {
+        Ordering::Less
+    } else if n > m {
+        Ordering::Greater
+    } else {
+        Ordering::Equal
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum TimeUnit {
+    Seconds,
+    Minutes,
+    Hours,
+    Days,
+    Months,
+    Years,
+}
+pub enum RoughTime {
+    InThePast(TimeUnit, u32),
+    JustNow,
+    InTheFuture(TimeUnit, u32),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _rt = RoughTime::InTheFuture(TimeUnit::Seconds, 2400);
 }
