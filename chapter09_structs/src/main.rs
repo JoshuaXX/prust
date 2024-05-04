1e644dbe56de9adddab6051b10337220e07f059e
tang joshua <tjh.xbow@qq.com>
2024-05-04 13:55:02 +0800

    add use directives

--- chapter09_structs/src/main.rs
+++ chapter09_structs/src/main.rs
@@ -1,9 +1,11 @@
+use chapter09_structs::*;
+
 fn main() {
-    let origin = chapter09_structs::PointTuple::new(0.0, 0.0, 0.0);
-    let p = chapter09_structs::PointTuple::new(2.0, 3.0, 4.0);
+    let origin = PointTuple::new(0.0, 0.0, 0.0);
+    let p = PointTuple::new(2.0, 3.0, 4.0);
     println!("distance of {:?} to {:?} is {}", origin, p, p.distance(&origin));
 
-    let origin = chapter09_structs::PointStruct::new(0.0, 0.0, 0.0);
-    let p = chapter09_structs::PointStruct::new(2.0, 3.0, 4.0);
+    let origin = PointStruct::new(0.0, 0.0, 0.0);
+    let p = PointStruct::new(2.0, 3.0, 4.0);
     println!("distance of {:?} to {:?} is {}", origin, p, p.distance(&origin));
 }
